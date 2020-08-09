7d20f6ae8e3e1ca7e5cf280594b268fd0d4c8d32
cfredberg <53453343+cfredberg@users.noreply.github.com>
2020-08-09 12:55:51 -0700

    up to slot IP

--- src/_11_whack_a_mole/whackanonrubikscube.java
+++ src/_11_whack_a_mole/whackanonrubikscube.java
@@ -1,28 +1,38 @@
 package _11_whack_a_mole;
 
+import java.applet.AudioClip;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Date;
 import java.util.Random;
 
+import javax.swing.JApplet;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class whackanonrubikscube implements ActionListener {
 	JFrame frame;
-	
-	int moleCount;
-	int missCount;
+	Date start = new Date();
+	int moleCount = 0;
+	int missCount = 0;
 	//24 buttons
 	public static void main(String[] args) {
 		whackanonrubikscube w = new whackanonrubikscube();
 		w.run();
 	}
 	
+	int ran2 = 0;
+	
 	public void run() {
-		moleCount = 0;
-		missCount = 0;
+		if(moleCount == 10) {
+			endGame(start, moleCount);
+		}else if(missCount == 5) {
+			JOptionPane.showMessageDialog(null, "You have lost");
+			System.exit(0);
+		}
 		frame = new JFrame("Whack a Non-Rubik's Cube");
 		JPanel panel = new JPanel();
 		frame.add(panel);
@@ -42,21 +52,36 @@ public class whackanonrubikscube implements ActionListener {
 				}else if (ran1 == 1) {
 					b.setText("Rubik's Cube");
 				}
+				ran2 = ran1;
 			}
 		}
 		frame.pack();
+	}
+	
+	private void endGame(Date timeAtStart, int molesWhacked) { 
+	    Date timeAtEnd = new Date();
+	    JOptionPane.showMessageDialog(null, "Your whack rate is "
+	            + ((timeAtEnd.getTime() - timeAtStart.getTime()) / 1000.00 / molesWhacked)
+	                  + " moles per second.");
+	    System.exit(0);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		JButton b1 = (JButton) e.getSource();
-		if (b1.getText().equals("")) {
+		if (b1.getText().equals("") && ran2 == 0) {
 			speak("You missed");
 			missCount++;
-		}else if (b1.getText().equals("IPhone")) {
+		}else if(b1.getText().equals("") && ran2 == 1) {
+			speak("Good Job");
+			moleCount++;
+			playSound("sound.wav");
+		}
+		else if (b1.getText().equals("IPhone")) {
 			speak("Good job");
 			moleCount++;
+			playSound("sound.wav");
 		}else if (b1.getText().equals("Rubik's Cube")) {
 			speak("You missed");
 			missCount++;
@@ -83,5 +108,10 @@ public class whackanonrubikscube implements ActionListener {
             }
         }
     }
+	
+	private void playSound(String fileName) { 
+	    AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName));
+	    sound.play();
+	}
 
 }
---
+++ src/_12_slot_machine/slot.java
@@ -0,0 +1,132 @@
+package _12_slot_machine;
+
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Random;
+
+import javax.swing.*;
+
+public class slot implements ActionListener {
+	public static void main(String[] args) {
+		slot s = new slot();
+		s.code();
+	}
+
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton button = new JButton("Spin");
+	JLabel pic1 = new JLabel();
+	JLabel pic2 = new JLabel();
+	JLabel pic3 = new JLabel();
+	int num1 = 0;
+	int num2 = 0;
+	int num3 = 0;
+	
+	private void code() {
+		// TODO Auto-generated method stub
+		frame.add(panel);
+		frame.setVisible(true);
+		frame.setPreferredSize(new Dimension(500, 300));
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		
+		button.addActionListener(this);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		num1 = new Random().nextInt(3);
+		num2 = new Random().nextInt(3);
+		num3 = new Random().nextInt(3);
+		if (num1 == 0) {
+			try {
+				pic1 = image("cpu1.jpg");
+			} catch (MalformedURLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}else if (num1 == 1) {
+			try {
+				pic1 = image("cpu2.jpg");
+			} catch (MalformedURLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}else if (num1 == 2) {
+			try {
+				pic1 = image("cpu3.jpg");
+			} catch (MalformedURLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+		
+		if (num2 == 0) {
+			try {
+				pic2 = image("cpu1.jpg");
+			} catch (MalformedURLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}else if (num2 == 1) {
+			try {
+				pic2 = image("cpu2.jpg");
+			} catch (MalformedURLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}else if (num2 == 2) {
+			try {
+				pic2 = image("cpu3.jpg");
+			} catch (MalformedURLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+		
+		if (num3 == 0) {
+			try {
+				pic3 = image("cpu1.jpg");
+			} catch (MalformedURLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}else if (num3 == 1) {
+			try {
+				pic3 = image("cpu2.jpg");
+			} catch (MalformedURLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}else if (num3 == 2) {
+			try {
+				pic3 = image("cpu3.jpg");
+			} catch (MalformedURLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+		
+		panel.add(button);
+		panel.add(pic1);
+		panel.add(pic2);
+		panel.add(pic3);
+		frame.pack();
+	}
+	
+	private JLabel image(String fileName) throws MalformedURLException{
+        URL imageURL = getClass().getResource(fileName);
+	if (imageURL == null){
+		System.err.println("Could not find image " + fileName);
+		return new JLabel();
+	}
+	Icon icon = new ImageIcon(imageURL);
+	JLabel imageLabel = new JLabel(icon);
+	return imageLabel;
+}
+
+}
